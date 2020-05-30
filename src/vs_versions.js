1de303e1b72b0bde84b4de368a737f30d2eeb40e
StuxCrystal (Roland Netzsch) <stuxcrystal@encode.moe>
2020-05-30 13:43:29 +0200

    Fix incorrect zimg version list

--- src/vs_versions.js
+++ src/vs_versions.js
@@ -1,6 +1,7 @@
 export const VS_ALIASES = {
     "latest": "50",
     "47": "47.2",
+    "45": "45.0",
     "42": "42.1"
 };
 
@@ -15,31 +16,31 @@ export const VS_VERSIONS = {
         minor: "49",
         pypi_version: "49",
         vs_branch: "R49",
-        zimg_branch: "v2.8"
+        zimg_branch: "v2.9"
     },
     "48": {
         minor: "48",
         pypi_version: "48",
         vs_branch: "R48",
-        zimg_branch: "v2.8"
+        zimg_branch: "v2.9"
     },
     "47.2": {
         pypi_version: "47.2",
         minor: "47.2",
         vs_branch: "R47.2",
-        zimg_branch: "v2.8"
+        zimg_branch: "v2.9"
     },
     "47.1": {
         pypi_version: "47.1",
         minor: "47.1",
         vs_branch: "R47.1",
-        zimg_branch: "v2.8"
+        zimg_branch: "v2.9"
     },
     "47.0": {
         pypi_version: "47",
         minor: "47",
         vs_branch: "R47",
-        zimg_branch: "v2.8"
+        zimg_branch: "v2.9"
     },
     "46": {
         minor: "46",
@@ -47,7 +48,13 @@ export const VS_VERSIONS = {
         vs_branch: "R46",
         zimg_branch: "v2.8"
     },
-    "45": {
+    "45.1": {
+        minor: "45.1",
+        pypi_version: "45",
+        vs_branch: "R45.1",
+        zimg_branch: "v2.8"
+    },
+    "45.0": {
         minor: "45",
         pypi_version: "45",
         vs_branch: "R45",
@@ -57,7 +64,7 @@ export const VS_VERSIONS = {
         minor: "44",
         pypi_version: "44",
         vs_branch: "R44",
-        zimg_branch: "v2.8"
+        zimg_branch: "v2.7"
     },
     "43": {
         minor: "43",
