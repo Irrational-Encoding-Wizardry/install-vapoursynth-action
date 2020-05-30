e408dde69ef06b6907379c4fb7228f46b3ab1ae8
StuxCrystal (Roland Netzsch) <stuxcrystal@encode.moe>
2020-05-30 02:15:00 +0200

    Set node as target

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: './src/index.js',
-
+    target: "node",
     output: {
         path: __dirname + "/dist",
         filename: 'index.js'
